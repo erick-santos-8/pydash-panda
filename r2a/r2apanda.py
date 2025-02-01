d6f5325a346ca745541f7bd37f4574e41fba3572
erick-santos-8 <esagosto@hotmail.com>
2025-02-01 19:12:13 -0300

    estimativa funcionando

--- r2a/r2apanda.py
+++ r2a/r2apanda.py
@@ -21,13 +21,13 @@ class R2APANDA(IR2A):
     def handle_xml_request(self, msg):
         """ Sends the XML request to the ConnectionHandler. """
 
-        self.tempo_ultima_solicitacao = time.perf_counter() # used to calculate the first throughput
+        self.tempo_ultima_solicitacao = perf_counter() # used to calculate the first throughput
         self.send_down(msg)
 
     def handle_xml_response(self, msg):
         
         # Taxa de transferencia é calculada pelo tamanho do segmento do video pelo tempo decorrido desde a ultima solicitação 
-        self.xtn_m1 = msg.get_bit_length() / (time.perf_counter() - self.tempo_ultima_solicitacao)
+        self.xtn_m1 = msg.get_bit_length() / (perf_counter() - self.tempo_ultima_solicitacao)
 
         # Ultiam estimativa de taxa de transferencia é igual a taxa de transferencia calculada
         self.xcn_m1 = self.xtn_m1
@@ -40,10 +40,10 @@ class R2APANDA(IR2A):
 
 
         # Recebe a lista com as qualidades do video
-        parse_mpd = parse_mpd(msg.get_payload())
+        parsed_mpd = parse_mpd(msg.get_payload())
 
         # Adiciona a lista de qualidades do video à lista de listas de qualidades do video
-        self.lista_segmentos = parse_mpd.get_qi()
+        self.lista_segmentos = parsed_mpd.get_qi()
 
 
         # Proxima função  
@@ -53,18 +53,25 @@ class R2APANDA(IR2A):
     def handle_segment_size_request(self, msg):
 
         # tempo decorrido desde a ultima solicitação
-        tempo_de_espera = time.perf_counter() - self.tempo_ultima_solicitacao
+        tempo_de_espera = perf_counter() - self.tempo_ultima_solicitacao
         
         # Se o tempo decorrido desde a ultima solicitação for menor que o tempo de espera, espera o tempo de espera restante
         if tempo_de_espera < self.tempo_proxima_solicitacao:
             sleep(self.tempo_proxima_solicitacao - tempo_de_espera)
 
-        solicitação_de_tempo_atual = time.perf_counter()
+        solicitação_de_tempo_atual = perf_counter()
         intervalo_tempo_solicitacao_atual = solicitação_de_tempo_atual - self.tempo_ultima_solicitacao
         self.tempo_ultima_solicitacao = solicitação_de_tempo_atual
 
         # Estimativa  
-        xcn = (self.k * (self.w - (self.xcn_m1 - self.xtn_m1 + self.w))) * intervalo_tempo_solicitacao_atual + xcn_m1
+        xcn = (self.k * (self.w - (self.xcn_m1 - self.xtn_m1 + self.w))) * intervalo_tempo_solicitacao_atual + self.xcn_m1
+        print('estimativa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: ', xcn)
 
+    def handle_segment_size_response(self, msg):
+        pass
 
-        
+    def initialize(self):
+        pass
+
+    def finalization(self):
+        pass 
